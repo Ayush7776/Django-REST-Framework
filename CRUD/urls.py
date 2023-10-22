beee5ab33ed40265f5bc1bbbf803ce56100d3127
Yash <ayushpathak7776@gmail.com>
2023-10-22 14:20:15 +0530

    Class Based Api View

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi/',views.student),
-    path('studentapi/<int:pk>',views.student),
+    path('studentapi/',views.StudentApi.as_view()),
+    path('studentapi/<int:pk>',views.StudentApi.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -3,35 +3,37 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Student
 from .serializer import StudentSerializer
+from rest_framework import status
+from rest_framework.views import APIView
 
 # Create your views here.
-@api_view(['POST','GET','PUT','DELETE'])
-def student(request,pk=None): 
-    if request.method=='GET':
+class StudentApi(APIView):
+
+    def get(self,request,pk=None,formate=None): 
         if pk is not None:
             stu=Student.objects.get(pk=pk)
             serializer=StudentSerializer(stu)
-            return Response(serializer.data)
+            return Response(serializer.data,status=status.HTTP_302_FOUND)
         stu=Student.objects.all()
         serializer=StudentSerializer(stu,many=True)
-        return Response(serializer.data)
+        return Response(serializer.data,status=status.HTTP_302_FOUND)
 
-    if request.method=='POST':
+    def post(self,request,pk=None,formate=None):
         serializer=StudentSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'msg':'Account Created'})
-        return Response(serializer.errors())
+            return Response({'msg':'Account Created'},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors(),status=status.HTTP_404_NOT_FOUND)
     
-    if request.method=='PUT':
+    def put(self,request,pk=None,formate=None):
         stu=Student.objects.get(pk=pk)
         serializer=StudentSerializer(stu,data=request.data,partial=True)
         if serializer.is_valid():
             serializer.save()
-            return Response({'msg':'Data Updated'})
-        return Response(serializer.errors())
+            return Response({'msg':'Data Updated'},status=status.HTTP_426_UPGRADE_REQUIRED)
+        return Response(serializer.errors(),status=status.HTTP_404_NOT_FOUND)
         
-    if request.method=="DELETE":
+    def delete(self,request,pk=None,formate=None):
         stu=Student.objects.get(pk=pk)
         stu.delete()
-        return Response({'msg':'Data Deleted'})
+        return Response({'msg':'Data Deleted'},status=status.HTTP_200_OK)
