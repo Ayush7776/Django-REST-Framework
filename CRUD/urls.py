a17afe75dcd317c3f8890dc5aa132a10aff48716
Yash <ayushpathak7776@gmail.com>
2023-10-23 18:16:29 +0530

    Generic API View & Mixins

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi/',views.StudentApi.as_view()),
-    path('studentapi/<int:pk>',views.StudentApi.as_view()),
+    path('studentapi/',views.StudentList.as_view()),
+    # path('studentapi/',views.StudentCreate.as_view()),
+    # path('studentapi/<int:pk>/',views.StudentRetrive.as_view()),
+    # path('studentapi/<int:pk>/',views.StudentUpdate.as_view()),
+    path('studentapi/<int:pk>/',views.StudentDestroy.as_view()),
+
 ]
--- api/views.py
+++ api/views.py
@@ -1,39 +1,39 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 from .models import Student
 from .serializer import StudentSerializer
-from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin
 
-# Create your views here.
-class StudentApi(APIView):
+class StudentList(GenericAPIView,ListModelMixin):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
 
-    def get(self,request,pk=None,formate=None): 
-        if pk is not None:
-            stu=Student.objects.get(pk=pk)
-            serializer=StudentSerializer(stu)
-            return Response(serializer.data,status=status.HTTP_302_FOUND)
-        stu=Student.objects.all()
-        serializer=StudentSerializer(stu,many=True)
-        return Response(serializer.data,status=status.HTTP_302_FOUND)
+    def get(self,request,*args,**kwargs):
+        return self.list(request,*args,**kwargs)
 
-    def post(self,request,pk=None,formate=None):
-        serializer=StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'msg':'Account Created'},status=status.HTTP_201_CREATED)
-        return Response(serializer.errors(),status=status.HTTP_404_NOT_FOUND)
-    
-    def put(self,request,pk=None,formate=None):
-        stu=Student.objects.get(pk=pk)
-        serializer=StudentSerializer(stu,data=request.data,partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'msg':'Data Updated'},status=status.HTTP_426_UPGRADE_REQUIRED)
-        return Response(serializer.errors(),status=status.HTTP_404_NOT_FOUND)
-        
-    def delete(self,request,pk=None,formate=None):
-        stu=Student.objects.get(pk=pk)
-        stu.delete()
-        return Response({'msg':'Data Deleted'},status=status.HTTP_200_OK)
+class StudentCreate(GenericAPIView,CreateModelMixin):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args,**kwargs)
+
+class StudentRetrive(GenericAPIView,RetrieveModelMixin):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+    def get(self,request,*args,**kwargs):
+        return self.retrieve(request,*args,**kwargs)
+
+class StudentUpdate(GenericAPIView,UpdateModelMixin):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+    def put(self,request,*args,**kwargs):
+        return self.update(request,*args,**kwargs)
+
+class StudentDestroy(GenericAPIView,DestroyModelMixin):
+    queryset=Student.objects.all()
+    serializer_class=StudentSerializer
+
+    def delete(self,request,*args,**kwargs):
+        return self.destroy(request,*args,**kwargs)
