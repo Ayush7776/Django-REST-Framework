70797edf10244a7cb8fdbd975d157a873013ba57
Yash <ayushpathak7776@gmail.com>
2023-10-28 11:56:56 +0530

    AllowAny IsAdminUser

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -124,8 +124,14 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Predefine Settings For All ModelViewSet Classes,We Have To Not Write For Each Class 
-REST_FRAMEWORK={
-    'DEFAULT_AUTHENTICATION_CLASSES':['rest_framework.authentication.BasicAuthentication'],
-    'DEFAULT_PERMISSION_CLASSES':['rest_framework.permissions.IsAuthenticated']
+# Predefine Settings For All ModelViewSet Classes,We Have To Not Write For Each 
+#Class 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
 }
--- api/views.py
+++ api/views.py
@@ -2,15 +2,16 @@ from .models import Student
 from .serializer import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser
 
 # Create your views here.
 class StudetModelViewSet(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
+    
+    authentication_classes=[BasicAuthentication]
+    # This Will Be Only Take Permission To Staff Status True In Admin Panel
+    permission_classes=[IsAdminUser]
 
-    # Basic Authentication For Access API
-    # authentication_classes=[BasicAuthentication]
-    # permission_classes=[IsAuthenticated]
 
     
