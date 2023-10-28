b09ae1639d8eac65e81492b137fdb59f627ed8e7
Yash <ayushpathak7776@gmail.com>
2023-10-28 11:15:46 +0530

    Basic Authentication For Access API

--- api/views.py
+++ api/views.py
@@ -1,10 +1,16 @@
 from .models import Student
 from .serializer import StudentSerializer
 from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
-class StudetModelViewSet(viewsets.ReadOnlyModelViewSet):
+class StudetModelViewSet(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
+    
+    # Basic Authentication For Access API
+    authentication_classes=[BasicAuthentication]
+    permission_classes=[IsAuthenticated]
 
     
