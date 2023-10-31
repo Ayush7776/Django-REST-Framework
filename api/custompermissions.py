658307e70446069f99694de8fc64a7ab02f091b9
Yash <ayushpathak7776@gmail.com>
2023-10-31 09:51:25 +0530

    Custom Permissions

---
+++ api/custompermissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission
+
+class Mypermissions(BasePermission):
+    def has_permission(self,request,view):
+        # if request.method=="POST":
+        if request.method=="POST" or request.method=="GET":
+            return True
+        else:
+            return False
--- api/views.py
+++ api/views.py
@@ -2,31 +2,11 @@ from .models import Student
 from .serializer import StudentSerializer
 from rest_framework import viewsets
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly,DjangoModelPermissions,DjangoModelPermissionsOrAnonReadOnly
-
+from .custompermissions import Mypermissions
 # Create your views here.
 class StudetModelViewSet(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
     
     authentication_classes=[SessionAuthentication]
-    # This Will Be Only Take Permission To Staff Status True In Admin Panel
-    # permission_classes=[IsAuthenticated]
-
-    # In AllowAny  Can Make Operation Authenticated Or NonAuthenticated User 
-    # permission_classes=[AllowAny]
-
-    # IsAdminUser Admin Can Make Operation And All User Cannot Access API Data 
-    # permission_classes=[IsAdminUser]
-
-    # In IsAuthenticatedOrReadOnly Authenticated User Can Make Operation And    UnAuthenticated User Can Read Only API Data 
-    # permission_classes=[IsAuthenticatedOrReadOnly]
-
-    # In DjangoModelPermissions We Have To Explicitly Take Permission From SuperUser Pannel
-    # permission_classes=[DjangoModelPermissions]
-
-    # In DjangoModelPermissionsOrAnonReadOnly We Have To Explicitly Take Permission From SuperUser Pannel And UnAuthorized User Can Read Only
-    # permission_classes=[DjangoModelPermissionsOrAnonReadOnly]
-
-
-    
+    permission_classes=[Mypermissions]
