0223ca65f1c340b9c89bb3de674618c5aff425f5
Yash <ayushpathak7776@gmail.com>
2023-11-02 18:15:16 +0530

    Creating Authtoken Using Admin Panal

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -1,9 +1,16 @@
 
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from api import views
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+
+router.register('studentapi',views.StudetModelViewSet,basename='student')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('studentapi/',views.student),
-    path('studentapi/<int:pk>',views.student),
+    path('',include(router.urls)),
+    path('auth',include('rest_framework.urls',namespace='rest_framework'))
 ]
