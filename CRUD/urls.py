c5702e339f8fb96a21d292f179649db0e7bd0356
Yash <ayushpathak7776@gmail.com>
2023-11-02 18:30:09 +0530

    Auth-Token Using Post Request (Thuder Client)

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path,include
 from api import views
 from rest_framework.routers import DefaultRouter
-
+from rest_framework.authtoken.views import obtain_auth_token
 router=DefaultRouter()
 
 router.register('studentapi',views.StudetModelViewSet,basename='student')
@@ -12,5 +12,6 @@ router.register('studentapi',views.StudetModelViewSet,basename='student')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
-    path('auth',include('rest_framework.urls',namespace='rest_framework'))
+    path('auth',include('rest_framework.urls',namespace='rest_framework')),
+    path('token/',obtain_auth_token),
 ]
