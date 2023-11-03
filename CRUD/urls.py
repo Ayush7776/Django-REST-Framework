cc40ca2b1f53e1f6e5dc1226e341a15092acec27
Yash <ayushpathak7776@gmail.com>
2023-11-03 17:04:21 +0530

    CustomAuthtoken With Responce

--- CRUD/urls.py
+++ CRUD/urls.py
@@ -1,10 +1,9 @@
-
-
+from api.auth import CustomAuthToken
 from django.contrib import admin
 from django.urls import path,include
 from api import views
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
+
 router=DefaultRouter()
 
 router.register('studentapi',views.StudetModelViewSet,basename='student')
@@ -13,5 +12,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls)),
     path('auth',include('rest_framework.urls',namespace='rest_framework')),
-    path('token/',obtain_auth_token),
+    path('token/',CustomAuthToken.as_view()),
 ]
