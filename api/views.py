fe0ff26123a14789608b1f6f158e485cef8f1128
Yash <ayushpathak7776@gmail.com>
2023-11-13 18:07:40 +0530

    ObtainAuthToken (Custom Return Responce)

--- api/views.py
+++ api/views.py
@@ -1,16 +1,11 @@
 from .models import Student
 from .serializer import StudentSerializer
 from rest_framework import viewsets
-from rest_framework.authentication import SessionAuthentication
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 class StudetModelViewSet(viewsets.ModelViewSet):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
-    
-    # We Can Creat Auth-Token Using Following Command 
-    # python manage.py drf_create_token (username) 
-
-
-    # authentication_classes=[SessionAuthentication]
-    # permission_classes=[IsAuthenticated]
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
